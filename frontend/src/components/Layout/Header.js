9a36beac0babf380bc75fad7c858c7661ddb9711
DIGONTA HALDER <digantahalder14000@gmail.com>
2023-10-16 15:30:13 +0530

    HeaderTable is added

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import './Header.css'; // Custom CSS
 import 'bootstrap/dist/css/bootstrap.min.css'; // Include Bootstrap CSS
+import HeaderTable from './HeaderTable';
 
 function Header() {
   return (
-    <header className="header bg-primary text-white">
-      <div className="container d-flex justify-content-between align-items-center">
-        <div className="header-left">
-          <h1 className="display-4">React-Medicine</h1>
-        </div>
-        <div className="header-right">
-          <div className="cart-button d-flex align-items-center">
-            <i className="bi bi-cart fs-4 me-2"></i> {/* Assuming you have Bootstrap icons */}
-            <button className="btn btn-light">Cart</button>
+    <>
+      <header className="header bg-primary text-white">
+        <div className="container d-flex justify-content-between align-items-center">
+          <div className="header-left">
+            <h1 className="display-4">React-Medicine</h1>
+          </div>
+          <div className="header-right">
+            <div className="cart-button d-flex align-items-center">
+              <i className="bi bi-cart fs-4 me-2"></i> {/* Assuming you have Bootstrap icons */}
+              <button className="btn btn-light">Cart</button>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <HeaderTable />
+    </>
   );
 }
 
---
+++ frontend/src/components/Layout/HeaderTable.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react';
+import './HeaderTable.css';
+const HeaderTable = () => {
+  const [medicineName, setMedicineName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [products, setProducts] = useState([]);
+
+  const handleAddProduct = () => {
+    if (medicineName && description && price && quantity) {
+      const newProduct = {
+        medicineName,
+        description,
+        price,
+        quantity,
+      };
+      setProducts([...products, newProduct]);
+      setMedicineName('');
+      setDescription('');
+      setPrice('');
+      setQuantity('');
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem('products', JSON.stringify(products));
+  }, [products]);
+
+  return (
+    <div className="container">
+      <h1>Add Medicine</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <section>
+            <h2>Medicine Name</h2>
+            <input
+              type="text"
+              value={medicineName}
+              onChange={(e) => setMedicineName(e.target.value)}
+            />
+          </section>
+          <section>
+            <h2>Description</h2>
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </section>
+          <section>
+            <h2>Price</h2>
+            <input
+              type="number"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </section>
+          <section>
+            <h2>Quantity</h2>
+            <input
+              type="number"
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+            />
+          </section>
+          <section>
+            <button className="btn btn-primary" onClick={handleAddProduct}>
+              Add Product
+            </button>
+          </section>
+        </div>
+        <div className="col-md-6">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Medicine Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Add To Cart</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => (
+                <tr key={index}>
+                  <td>{product.medicineName}</td>
+                  <td>{product.description}</td>
+                  <td>{product.price}</td>
+                  <td>{product.quantity}</td>
+                  <td>
+                    <button className="btn btn-success">Add To Cart</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderTable;
